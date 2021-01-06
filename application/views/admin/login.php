66757287c34a57740a16d86b73ce025b7fb2d9b7
Halimah <32287174+halimahdiah@users.noreply.github.com>
2021-01-06 13:34:21 +0800

    login bisa

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -24,10 +24,43 @@
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in</p>
+      
+          <?php
+              $errors = $this->session->flashdata('errors');
+              if(!empty($errors)){
+              ?>
+                <div class="row">
+                    <div class="col-md-12">
+                    <div class="alert alert-danger text-center">
+                        <?php foreach($errors as $key=>$error){ ?>
+                        <?php echo "$error<br>"; ?>
+                        <?php } ?>
+                    </div>
+                    </div>
+                </div>
+              <?php
+              }
+              if($msg = $this->session->flashdata('error_login')){ ?>
+                <div class="row">
+                    <div class="col-md-12">
+                      <div class="alert alert-danger text-center">
+                          <?= $msg ?>
+                      </div>
+                    </div>
+                </div>
+              <?php } else if($msg = $this->session->flashdata('success_login')){ ?>
+                <div class="row">
+                    <div class="col-md-12">
+                      <div class="alert alert-success text-center">
+                          <?= $msg ?>
+                      </div>
+                    </div>
+                </div>
+              <?php } ?>
 
-      <form action="<?php echo site_url('Siswa')?>" method="post">
+      <form action="<?= base_url('auth/login') ?>" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Username">
+          <input name="username" type="text" class="form-control" placeholder="Username">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -35,7 +68,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input name="password" type="password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
